1d6f2587fd38f255255fd1fe4f27f1362cb531df
hardik dhiman <hardikdhiman.hd6@gmail.com>
2020-04-15 22:04:57 +0530

    commit 2

--- proman-api/target/generated-sources/io/swagger/api/ApiResponseMessage.java
+++ proman-api/target/generated-sources/io/swagger/api/ApiResponseMessage.java
@@ -2,7 +2,7 @@ package io.swagger.api;
 
 import javax.xml.bind.annotation.XmlTransient;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2020-04-13T21:40:29.098+05:30")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2020-04-14T22:28:44.714+05:30")
 
 @javax.xml.bind.annotation.XmlRootElement
 public class ApiResponseMessage {
--- proman-api/target/generated-sources/io/swagger/configuration/SwaggerDocumentationConfig.java
+++ proman-api/target/generated-sources/io/swagger/configuration/SwaggerDocumentationConfig.java
@@ -10,7 +10,7 @@ import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2020-04-13T21:40:29.098+05:30")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2020-04-14T22:28:44.714+05:30")
 
 @Configuration
 public class SwaggerDocumentationConfig {
